a452654aba89519b04dd5f7c05034b2f07550f47
Sultan Alsawaf <sultanxda@gmail.com>
2018-05-13 14:54:21 -0700

    input: touchscreen: Offload fb notifier callbacks onto workers
    Framebuffer notifier callbacks block framebuffer state changes until
    the callbacks are finished executing. This means that any significant
    delays in a framebuffer notifier callback will stall the screen when
    powering it on.
    
    In order to eliminate screen-on delays from these framebuffer notifiers,
    offload their callbacks onto workers.
    
    Change-Id: Ia40aea9c4fe7dc7e714d3916a82d3c998c1da12a
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>

--- drivers/input/touchscreen/stm/ftm4_ts.c
+++ drivers/input/touchscreen/stm/ftm4_ts.c
@@ -100,6 +100,7 @@ static int fts_suspend(struct i2c_client *client, pm_message_t mesg);
 static int fts_resume(struct i2c_client *client);
 
 #if defined(CONFIG_FB)
+static void touch_pm_worker(struct work_struct *work);
 static int touch_fb_notifier_callback(struct notifier_block *self,
 		unsigned long event, void *data);
 #endif
@@ -1964,6 +1965,7 @@ static int fts_probe(struct i2c_client *client, const struct i2c_device_id *idp)
 #endif
 
 #ifdef CONFIG_FB
+	INIT_WORK(&info->pm_work, touch_pm_worker);
 	info->fb_notif.notifier_call = touch_fb_notifier_callback;
 	retval = fb_register_client(&info->fb_notif);
 	if (retval < 0) {
@@ -2534,6 +2536,16 @@ exit:
 }
 
 #if defined(CONFIG_FB)
+static void touch_pm_worker(struct work_struct *work)
+{
+	struct fts_ts_info *info = container_of(work, typeof(*info), pm_work);
+
+	if (info->screen_off)
+		fts_suspend(info->client, PMSG_SUSPEND);
+	else
+		fts_resume(info->client);
+}
+
 static int touch_fb_notifier_callback(struct notifier_block *self,
 		unsigned long event, void *data)
 {
@@ -2544,10 +2556,8 @@ static int touch_fb_notifier_callback(struct notifier_block *self,
 	if (ev && ev->data && event == FB_EVENT_BLANK) {
 		int *blank = (int *)ev->data;
 
-		if (*blank == FB_BLANK_UNBLANK)
-			fts_resume(info->client);
-		else
-			fts_suspend(info->client, PMSG_SUSPEND);
+		info->screen_off = *blank != FB_BLANK_UNBLANK;
+		schedule_work(&info->pm_work);
 	}
 
 	return 0;
