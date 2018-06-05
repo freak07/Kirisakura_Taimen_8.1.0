45b6f4d6fbe5d78d8ac1338084e572d58797524e
Sultan Alsawaf <sultanxda@gmail.com>
2018-06-05 15:13:00 -0700

    msm: mdss: Power on display asynchronously as early as possible
    Currently, mdss powers on the display a long time after an unblank is
    requested and gets completely blocked while powering on the display, so if
    the display takes a very long time to turn on, then mdss will be stuck
    unable to do anything else in the meantime. This results in a long delay
    between trying to wake the device and the display actually powering on
    and rendering a frame.
    
    In order to make the display turn on faster when waking the device from
    sleep, start powering on the display as soon as the framebuffer unblank
    event is received. This allows mdss to continue resuming while the display
    takes its time powering on. A high-priority kthread is used here to ensure
    the display powers on as quickly as possible.
    
    To make this work, kickoffs need to be blocked when they attempt to power
    on the display, so that a kickoff won't continue while the display is
    still powered off.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    (cherry picked from commit ce1369aafac06e84d23c0dec6da37d61e3ba6689)

--- drivers/video/fbdev/msm/mdss_dsi.h
+++ drivers/video/fbdev/msm/mdss_dsi.h
@@ -647,6 +647,13 @@ struct mdss_dsi_ctrl_pdata {
 	struct dsi_cmd_pos rgb_gain_pos;
 	struct rgb_gain rgb_gain;
 
+	struct notifier_block wake_notif;
+	struct task_struct *wake_thread;
+	struct completion wake_comp;
+	wait_queue_head_t wake_waitq;
+	atomic_t disp_is_on;
+	atomic_t needs_wake;
+
 	/* HBM */
 	struct dsi_panel_cmds hbm_on_cmds;
 	struct dsi_panel_cmds hbm_off_cmds;
