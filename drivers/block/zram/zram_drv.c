nathanchance/wahoo

Revert "wahoo: Remove overflow checking changes"
The real cause of the panics has been identified, bring this back for
hardening reasons.

This reverts commit 69a3323552cc6313b967a9d20f78fd15a7604040.

Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

drivers/block/zram/zram_drv.c
@@ -515,7 +515,7 @@ static struct zram_meta *zram_meta_alloc(char *pool_name, u64 disksize)
 		return NULL;
 
 	num_pages = disksize >> PAGE_SHIFT;
-	meta->table = vzalloc(num_pages * sizeof(*meta->table));
+	meta->table = vzalloc(array_size(num_pages, sizeof(*meta->table)));
 	if (!meta->table) {
 		pr_err("Error allocating zram address table\n");
 		goto out_error;

freak07/Kirisakura_Taimen_8.1.0

(cherry picked from commit a5010e1f533e0fe4b3e4aec82cdfef79d7ba6d6d)

drivers/block/zram/zram_drv.c
@@ -932,7 +932,7 @@ static bool zram_meta_alloc(struct zram *zram, u64 disksize)
 	size_t num_pages;
 
 	num_pages = disksize >> PAGE_SHIFT;
-	zram->table = vzalloc(num_pages * sizeof(*zram->table));
+	zram->table = vzalloc(array_size(num_pages, sizeof(*zram->table)));
 	if (!zram->table)
 		return false;
 